f20d1ffdbe225369e92ed2dbf7271cf493bb93ac
kevad95 <kevad95@gmail.com>
2017-03-22 10:44:52 +0100

    some DB shit

--- bookend/config/database.php
+++ bookend/config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'Skaiper7'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
