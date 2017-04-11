fe6e39c809d213be2178a880eb7567cc91148f11
kevad95 <kevad95@gmail.com>
2017-04-11 10:35:08 +0200

    bruker liste

---
+++ bookend/resources/views/user_list.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="{{ asset('css/signin.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/navbar-fixed-side.css') }}" rel="stylesheet">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <link class="jsbin" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
+  <script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  <script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js"></script>
+
+  <!--[if IE]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			@include ('layouts.sidebar')
+		</div>
+	</div>
+@include ('layouts.footer')
+</body>
+</html>
