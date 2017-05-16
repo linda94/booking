64436cec177780e8d02901294e49c71d8c6eeb68
kevad95 <kevad95@gmail.com>
2017-05-16 13:24:59 +0200

    route fixes

--- routes/web.php
+++ routes/web.php
@@ -10,42 +10,31 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/rooms', function () {
-
-	$rooms = DB::table('room')->get();
-
-    return view('rooms', ['rooms' => $rooms]);
-}); 
-*/
 use App\Room;
 use App\User;
 use App\Role;
 
+// Auth rammeverkets routes
 Auth::routes();
 
-Route::get('/rooms/edit_room/{room}', 'RoomController@index2')->name('editroom_redirect'); 
-//Route::get('rooms/{room}/edit', 'RoomController@edit');
+//logut
 Route::get('/auth/passwords/email', function () {
 	Auth::logout();
 	Session::flush();
 	return view('/auth/passwords/email');
 });
-/*  -- Ikke lagt inn --
-Route::get('/auth/passwords/reset', function () {
-	$success = "Passordet har blitt endret";
-	return view('/')->with(compact('success'));
-});
-*/
+Route::get('/logout', 'UserController@logout')->name('logout');
 
+// Rooms
 Route::get('/rooms/edit_room/{room}', 'RoomController@index2')->name('editroom_redirect');
 Route::get('rooms/{room}/edit', 'RoomController@edit');
 Route::PUT('/rooms/{room}', 'RoomController@update')->name('update');
 Route::delete('/rooms/{room}', 'RoomController@destroy')->name('delete');
+Route::get('/rooms/edit_room/{room}', 'RoomController@index2')->name('editroom_redirect'); 
+Route::get('/rooms', 'RoomController@index'); 
+Route::get('/rooms/{room}', 'RoomController@show')->name('room_profile'); 
+Route::patch('/rooms/{room}/', 'RoomController@update');
 
 // BookingController
 Route::get('/bookingV', 'BookingController@index');
@@ -54,33 +43,28 @@ Route::DELETE('/bookingV/{booking}', 'BookingController@destroy')->name('delete_
 Route::get('/bookingV/{booking}', 'BookingController@show')->name('show_booking');
 Route::PUT('/bookingV/{booking}', 'BookingController@update');
 
+// Root routes
 Route::get('/', 'WelcomeController@index');
 Route::get('/auth/login', 'WelcomeController@loginIndex');
 
-Route::get('/rooms', 'RoomController@index'); 
-Route::get('/rooms/{room}', 'RoomController@show')->name('room_profile'); 
-
+// Nytt rom
 Route::get('/newroom', 'RoomController@create');
 Route::get('/editroom', 'RoomController@editroom');
-
 Route::get('/newroom', 'SidebarController@create');
 
+// Invitere ny bruker og glemt passord
 Route::get('/invite_user', 'InviteUserController@index')->name('invite_user_index');
 Route::post('/emails/send', 'InviteUserController@send')->name('invite_mail'); 
-
-Route::get('/logout', 'UserController@logout')->name('logout');
 	
+// bruker profil
 Route::get('/users/home', 'HomeController@index')->name('users_return'); 
 Route::get('/users/home_edit', 'HomeController@index2'); 
 Route::get('/users/{users}', 'HomeController@show')->name('users_profile');
-
 Route::PUT('/users/home/{users}', 'HomeController@update')->name('update_user');
 Route::delete('/users/home/{users}', 'HomeController@destroy')->name('delete_user');
 
-Route::patch('/rooms/{room}/', 'RoomController@update');
-
+// Rom og bruker liste sidene
 Route::get('/room_list', 'RoomListController@index');
-
 Route::get('/user_list', 'UserListController@index'); // nr 1 
 Route::get('/user_list/{users}', 'UserListController@show')->name('users_profiles'); // nr 2
 Route::get('/users/user_home_edit/{users}','UserListController@index2'); // nr 3
