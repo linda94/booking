479f022b2abdc395cb9ff510ec5863943a888e18
kurrio <danielconnery1@hotmail.com>
2017-05-26 18:42:10 +0200

    siste master merge

--- config/database.php
+++ config/database.php
@@ -1,5 +1,13 @@
 <?php
+
+        $url = parse_url(getenv("DATABASE_URL"));
+        $host = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $database = substr($url["path"], 1);
+
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -10,7 +18,9 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'default' => env('DB_CONNECTION', 'pgsql'),
+
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -26,12 +36,15 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
+
     'connections' => [
+
         'sqlite' => [
             'driver' => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -45,19 +58,22 @@ return [
             'strict' => true,
             'engine' => null,
         ],
+
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+
     ],
+
     /*
     |--------------------------------------------------------------------------
     | Migration Repository Table
@@ -68,7 +84,9 @@ return [
     | the migrations on disk haven't actually been run in the database.
     |
     */
+
     'migrations' => 'migrations',
+
     /*
     |--------------------------------------------------------------------------
     | Redis Databases
@@ -79,13 +97,18 @@ return [
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
+
     'redis' => [
+
         'client' => 'predis',
+
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
+
     ],
+
 ];
